9da51c30dcf7601d15d28d749d141580b25ffd85
Aditya Sarin <aditya28sarin@gmail.com>
2020-11-22 16:06:43 +0530

    Update 6, Nov 22, 2020

--- DSA/Searching Algorithms/Binary Search/bishu_and_soldiers.cpp
+++ DSA/Searching Algorithms/Binary Search/bishu_and_soldiers.cpp
@@ -1,4 +1,9 @@
-// Bishu went to fight for Coding Club. There were N soldiers with various powers. There will be Q rounds to fight and in each round Bishu's power will be varied. With power M, Bishu can kill all the soldiers whose power is less than or equal to M(<=M). After each round, All the soldiers who are dead in previous round will reborn.Such that in each round there will be N soldiers to fight. As Bishu is weak in mathematics, help him to count the number of soldiers that he can kill in each round and total sum of their powers.
+// Bishu went to fight for Coding Club. There were N soldiers with various powers.
+ // There will be Q rounds to fight and in each round Bishu's power will be varied.
+ // With power M, Bishu can kill all the soldiers whose power is less than or equal to M(<=M). 
+ // After each round, All the soldiers who are dead in previous round will reborn.Such that in 
+ // each round there will be N soldiers to fight. As Bishu is weak in mathematics, help him to
+ // count the number of soldiers that he can kill in each round and total sum of their powers.
 
 // 1<=N<=10000
 
