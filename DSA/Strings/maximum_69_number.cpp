7276f3c0b5ed5744bfdaed0019d444c8baccdc64
Aditya Sarin <aditya28sarin@gmail.com>
2020-11-27 16:08:32 +0530

    Update 14

---
+++ DSA/Strings/maximum_69_number.cpp
@@ -0,0 +1,39 @@
+// Given a positive integer num consisting only of digits 6 and 9.
+
+// Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+ 
+
+// Example 1:
+
+// Input: num = 9669
+// Output: 9969
+// Explanation: 
+// Changing the first digit results in 6669.
+// Changing the second digit results in 9969.
+// Changing the third digit results in 9699.
+// Changing the fourth digit results in 9666. 
+// The maximum number is 9969.
+
+-------------------------------------------------------------------------------------------------------
+						CODE
+-------------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    int maximum69Number (int num) 
+    {
+    string s = to_string(num);
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '6')
+            {
+                s[i] = '9';
+                break;
+            }
+        }
+        stringstream ss(s);
+        ss>>num;
+        return num;
+}
+};
