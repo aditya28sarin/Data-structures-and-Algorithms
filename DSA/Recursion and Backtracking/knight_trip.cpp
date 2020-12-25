132d77090f25f41a919ed2d80a3394b7232e08fb
Aditya Sarin <aditya28sarin@gmail.com>
2020-12-25 12:21:12 +0530

    Update 39

--- DSA/Recursion and Backtracking/knight_trip.cpp
+++ DSA/Recursion and Backtracking/knight_trip.cpp
@@ -1,1 +1,54 @@
 // URL: https://www.geeksforgeeks.org/the-knights-tour-problem-backtracking-1/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isValid(int arr[][5], int i, int j)
+{
+	return (i>=0 && j>=0 && i<5 && j<5 && arr[i][j]==0);
+}
+
+void knightProblem(int arr[][5], int i, int j)
+{
+	static int count = 0;
+
+	if(!isValid(arr,i,j))
+		return;
+
+	count++;
+
+	arr[i][j]=count;
+
+	knightProblem(arr,i+2,j+1);
+	knightProblem(arr,i+2,j-1);
+	knightProblem(arr,i-2,j+1);
+	knightProblem(arr,i-2,j-1);
+	knightProblem(arr,i+1,j+2);
+	knightProblem(arr,i+1,j-2);
+	knightProblem(arr,i-1,j+2);
+	knightProblem(arr,i-1,j-2);
+}
+
+int main()
+{
+		int maze[5][5]  =  {
+	   {0, 0, 0, 0, 0},
+	   {0, 0, 0, 0, 0},
+	   {0, 0, 0, 0, 0},
+	   {0, 0, 0, 0, 0},
+	   {0, 0, 0, 0, 0}
+	};
+
+	knightProblem(maze,0,0);
+
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+			cout<<maze[i][j]<<" ";
+		}
+
+		cout<<"\n";
+	}
+}
