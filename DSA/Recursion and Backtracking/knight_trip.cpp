25d27086aeadac06122079df7d80bae44e527697
Aditya Sarin <aditya28sarin@gmail.com>
2020-12-25 14:54:01 +0530

    Update 39

--- DSA/Recursion and Backtracking/knight_trip.cpp
+++ DSA/Recursion and Backtracking/knight_trip.cpp
@@ -4,51 +4,57 @@
 using namespace std;
 
 
-bool isValid(int arr[][5], int i, int j)
+// bool isValid(int arr[5][5], int i, int j)
+// {
+// 	return (i>=0 && j>=0 && i<5 && j<5 && arr[i][j]==0);
+// }
+
+
+void displayBoard(int arr[5][5])
 {
-	return (i>=0 && j>=0 && i<5 && j<5 && arr[i][j]==0);
-}
-
-void knightProblem(int arr[][5], int i, int j)
-{
-	static int count = 0;
-
-	if(!isValid(arr,i,j))
-		return;
-
-	count++;
-
-	arr[i][j]=count;
-
-	knightProblem(arr,i+2,j+1);
-	knightProblem(arr,i+2,j-1);
-	knightProblem(arr,i-2,j+1);
-	knightProblem(arr,i-2,j-1);
-	knightProblem(arr,i+1,j+2);
-	knightProblem(arr,i+1,j-2);
-	knightProblem(arr,i-1,j+2);
-	knightProblem(arr,i-1,j-2);
-}
-
-int main()
-{
-		int maze[5][5]  =  {
-	   {0, 0, 0, 0, 0},
-	   {0, 0, 0, 0, 0},
-	   {0, 0, 0, 0, 0},
-	   {0, 0, 0, 0, 0},
-	   {0, 0, 0, 0, 0}
-	};
-
-	knightProblem(maze,0,0);
-
 	for(int i=0;i<5;i++)
 	{
 		for(int j=0;j<5;j++)
 		{
-			cout<<maze[i][j]<<" ";
+			cout<<arr[i][j]<<" ";
 		}
-
 		cout<<"\n";
 	}
+}
+
+
+void knightProblem(int arr[5][5], int i, int j, int count)
+{
+
+	if(i<0 || j<0 || i>=5 || j>=5 || arr[i][j]!=0)
+		return;
+
+	else if(count==25)
+	{
+		arr[i][j]=count;
+		displayBoard(arr);
+		return;
+	}
+
+	arr[i][j]=count;
+
+	knightProblem(arr,i+2,j+1,count+1);
+	knightProblem(arr,i+2,j-1,count+1);
+	knightProblem(arr,i-2,j+1,count+1);
+	knightProblem(arr,i-2,j-1,count+1);
+	knightProblem(arr,i+1,j+2,count+1);
+	knightProblem(arr,i+1,j-2,count+1);
+	knightProblem(arr,i-1,j+2,count+1);
+	knightProblem(arr,i-1,j-2,count+1);
+
+	arr[i][j]=0;
+}
+
+
+int main()
+{
+
+	int arr[5][5]={0};
+
+	knightProblem(arr,0,0,1);
 }
