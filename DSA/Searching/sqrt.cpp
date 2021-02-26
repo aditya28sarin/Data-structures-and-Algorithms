fe96dad2fd9fbdf8a7001a4f50f09a5ac409253c
Aditya Sarin <aditya28sarin@gmail.com>
2021-02-26 13:37:47 +0530

    Update 83

---
+++ DSA/Searching/sqrt.cpp
@@ -0,0 +1,24 @@
+// URL: https://leetcode.com/problems/sqrtx/
+
+class Solution {
+public:
+    
+   int mySqrt(int x)
+{
+    long mid ;  // because we have to do mid*mid 
+    if(x ==0 || x==1) return x;
+    
+    int i=0, j=x ;
+    while(i<j)
+    {
+        mid = (i+j)/2;
+        
+        if(mid*mid == x)  return mid;
+        else if(mid*mid < x) i = mid;
+        else j=mid;
+        
+         if(j-i==1) return i;   
+    }
+    return i;
+}
+};
