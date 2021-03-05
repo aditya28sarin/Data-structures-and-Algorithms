74b839a7b79bc86dff53e48fbffcd3f3b631359c
Aditya Sarin <aditya28sarin@gmail.com>
2021-03-05 23:49:00 +0530

    Update 101

---
+++ DSA/stack/validate_stack_sequences.cpp
@@ -0,0 +1,16 @@
+//URL:https://leetcode.com/problems/validate-stack-sequences/
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int>s;
+        int i=0;
+        for (int j=0;j<pushed.size();++j) {
+            s.push(pushed[j]);
+
+            while (!s.empty() && s.top() == popped[i]) s.pop(), ++i;
+        }
+        
+
+        return s.empty();
+    }
+};
