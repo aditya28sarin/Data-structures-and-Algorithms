51f2ef8c2aae1ad55ac8832ff7696885ef755c5b
Aditya Sarin <aditya28sarin@gmail.com>
2021-03-06 21:13:11 +0530

    Update 105

---
+++ DSA/Searching/guess_higher_or_lower.cpp
@@ -0,0 +1,36 @@
+//URL: https://leetcode.com/problems/guess-number-higher-or-lower/
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l=1;
+        int r=n;
+
+        while(l<=r){
+            int m = (r-l)/2 + l;
+
+            if(guess(m)==0){
+                return m;
+            }
+
+            else if(guess(m)==-1){
+                r=m-1;
+            }
+            else
+                l=m+1;
+        }
+
+     return -1;
+    }
+
+
+};
