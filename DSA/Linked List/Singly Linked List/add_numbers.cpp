6a3ef0a93fe9d3ec0cfd84b1a83bc63858fdd2e0
Aditya Sarin <aditya28sarin@gmail.com>
2021-03-13 15:18:34 +0530

    Update 110

--- DSA/Linked List/Singly Linked List/add_numbers.cpp
+++ DSA/Linked List/Singly Linked List/add_numbers.cpp
@@ -1,5 +1,4 @@
 // URL: https://leetcode.com/problems/add-two-numbers/
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,33 +11,103 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        ListNode* dummy = new ListNode();
-        ListNode* temp = dummy;
-        
-        int carry=0;
-        
-        while(l1!=NULL || l2!=NULL || carry){
-            int sum = 0;
-            
-            if(l1!=NULL){
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            
-            
-            if(l2!=NULL){
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            
-            sum+=carry;
-            carry=sum/10;
-            ListNode* new_node = new ListNode(sum%10);
-            temp->next=new_node;
-            temp=temp->next;
-        }
-        return dummy->next;
+    
+    
+void insertAtEnd(ListNode** head_ref, int data)
+{
+    ListNode* new_node=new ListNode();
+    ListNode* prev_node=(*head_ref);
+    
+    new_node->val=data;
+
+    new_node->next=NULL;
+    
+    if((*head_ref)==NULL)
+        (*head_ref)=new_node;
+    else 
+    {
+        while(prev_node->next!=NULL)
+            prev_node=prev_node->next;
+        prev_node->next=new_node;
     }
+}
+    
+    
+ListNode* addTwoNumbers(ListNode* head1, ListNode* head2) {
+        
+    //if list 1 is empty simply return list 2
+    if(!head1){
+        return head2;
+    }
+    
+    //if list 2 is empty simply return list 1
+    if(!head2){
+        return head1;
+    }
+    
+    //check which list is longer in length and make that head1
+    ListNode* head_ref1=head1;
+    ListNode* head_ref2=head2;
+    int size1=0,size2=0;
+    while(head_ref1!=NULL){
+        size1++;
+        head_ref1=head_ref1->next;
+    }
+
+     while(head_ref2!=NULL){
+        size2++;
+        head_ref2=head_ref2->next;
+    }
+
+    if(size2>size1){
+        ListNode* temp;
+        temp=head1;
+        head1=head2;
+        head2=temp;
+    }
+       
+    
+    ListNode* temp1=head1;
+    ListNode* temp2 =head2;
+    int cry=0; //carry
+    
+    while(temp1){
+        
+        if(temp1 && temp2){
+            if((temp1->val+temp2->val+cry)/10==0){
+                temp1->val = temp1->val + temp2->val + cry;
+                cry=0;
+            }
+            else if((temp1->val + temp2->val +cry)/10!=0){
+               int data = (temp1->val + temp2->val + cry)%10;
+                cry=(temp1->val + temp2->val + cry)/10;
+                
+                temp1->val=data;
+            }
+            
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }else{
+            if((temp1->val+cry)/10==0){
+                temp1->val = temp1->val + cry;
+                cry=0;
+            }
+            else if((temp1->val +cry)/10!=0){
+                int data  = (temp1->val + cry)%10;
+                cry= (temp1->val + cry)/10;
+                
+                temp1->val=data;
+            }
+            temp1=temp1->next;
+        }
+        
+    }
+   
+    //if carry is not 0 then insert a node at end with value 1
+    if(cry!=0){
+        insertAtEnd(&head1,cry);
+    }
+    
+    return head1;   
+    }      
 };
