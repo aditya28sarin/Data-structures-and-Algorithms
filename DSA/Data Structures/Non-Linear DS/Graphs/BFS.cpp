6f4c726722ee4a369f8a62e993df812e41e1b6ff
Aditya Sarin <aditya28sarin@gmail.com>
2021-03-25 14:27:57 +0530

    Update 124

--- DSA/Data Structures/Non-Linear DS/Graphs/BFS.cpp
+++ DSA/Data Structures/Non-Linear DS/Graphs/BFS.cpp
@@ -1,150 +1,81 @@
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// class Graph{
-// public:
-// 	int vertex;
+class Graph{
+    int V;
+    list<int> *adj;
+    public:
+        Graph(int V){
+            this->V=V;
+            adj = new list<int>[V];
+        }
 
-// 	list<int> *adjList;
-
-// 	Graph(int v)
-// 	{
-// 		vertex=v;
-// 		adjList=new list<int>[v]; 
-// 	}
-
-// 	void addVertex(int u, int v, bool bidr=true)
-// 	{
-// 		adjList[u].push_back(v);
-		
-// 		if(bidr)
-// 		{
-// 			adjList[v].push_back(u);
-// 		}
-// 	}	
- 
-// 	void printAdjList()
-// 	{
-// 		for(int i=0;i<vertex;i++)
-// 		{
-// 			cout<<i<<"->";
-// 			for(int node:adjList[i])
-// 			{
-// 				cout<<node<<",";
-// 			}
-// 			cout<<endl;
-// 		}
-// 	}
-
-//     void BFS(int start)
-//     {
-//         queue<int> q;
-
-//         bool *visited = new bool[vertex+1]{0};
-
-//         q.push(start);
-//         visited[start]=true;
-
-//         while(!q.empty())
-//         {
-//             int temp = q.front();
-//             cout<<temp<<" ";
-//             q.pop();
-
-//             for(int neighbor: adjList[temp])
-//             {
-//                 if(!visited[neighbor])
-//                 {
-//                     q.push(neighbor);
-//                     visited[neighbor]=true;
-//                 }
-//             }
-//         }
-//     }
-// };
+        void addEdge(int x, int y){
+            adj[x].push_back(y);
+        }
+        void BFS(int source){
 
 
-// int main()
-// {
-// 	Graph g(6);
+            bool *visited = new bool[V];
+            for(int i=0;i<V;i++){
+                visited[i]=false;
+            }
+            
+            visited[source]=true;
+            queue<int> q;
+            q.push(source);
 
-// 	g.addVertex(0,1);
-// 	g.addVertex(1,2);
-// 	g.addVertex(0,4);
-// 	g.addVertex(2,4);
-// 	g.addVertex(3,2);
-// 	g.addVertex(2,3);
-// 	g.addVertex(3,5);
+            list<int> :: iterator it;
 
-// 	//g.printAdjList();	
-// 	g.BFS(0);
-// }
+            while(!q.empty()){
+                int x = q.front();
+                cout<<x<<" ";
+                q.pop();
 
+                for(it=adj[x].begin();it!=adj[x].end();++it){
+
+                    if(!visited[*it]){
+                        visited[*it]=true;
+                        q.push(*it);
+                    }
+                }
+            }
+        }
+};
 
 
 
 
 //map style 
-
-#include<bits/stdc++.h>
-using namespace std;
-
-
-class Graph{
+class Graph1 {
 public:
     int vertex;
+    unordered_map<int,list<int>> adjList;
 
-   map<int, list<int>> adjList;
+    void addEdge(int x, int y, int bidir=true){
+        adjList[x].push_back(y);
 
-    Graph()
-    {
-
-    }
-    
-    void addEdge(int u, int v, int bidir=true)
-    {
-        adjList[u].push_back(v);
-
-        if(bidir)
-        {
-            adjList[v].push_back(u);
+        if(bidir){
+            adjList[y].push_back(x);
         }
     }
 
-    void printList()
-    {       
-        for(auto row: adjList)
-        {
-      
-            int key = row.first;
-            cout<<key<<"->";
+    void BFS1(int start){
 
-            for(auto row_contents:row.second)
-            {
-                cout<<row_contents<<",";
-            }
-            cout<<endl;
-        }
-    }
-
-    void BFS(int start)
-    {
         queue<int> q;
-        bool *visited=new bool[adjList.size()+1]{0};
+        bool *visited = new bool[adjList.size()+1]{0};
         q.push(start);
-        visited[start]=true;
-        while(!q.empty())
-        {
-            int temp = q.front();
-            cout<<temp<<" ";
-             q.pop();
+        visited[start] = true;
 
-            for(auto val : adjList[temp])
-            {
-                if(!visited[val])
-                {
-                    q.push(val);
-                    visited[val]=true;
+        while(!q.empty()){
+            int x = q.front();
+            cout<<x<<" ";
+            q.pop();
+
+            for(auto element:adjList[x]){
+                if(!visited[element]){
+                    visited[element]=true;
+                    q.push(element);
                 }
             }
         }
@@ -153,21 +84,32 @@ public:
 };
 
 
-int main()
-{
 
-    Graph g;
-
-
+int main(){
+    Graph g(4);
     g.addEdge(0,1);
-	g.addEdge(1,2);
-	g.addEdge(0,4);
-	g.addEdge(2,4);
-	g.addEdge(3,2);
-	g.addEdge(2,3);
-	g.addEdge(3,5);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+ 
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) \n";
+    g.BFS(2);
 
-    g.printList();
 
-    g.BFS(0);
+    cout<<endl;
+
+    Graph1 g1;
+
+    g1.addEdge(0,1);
+	g1.addEdge(1,2);
+	g1.addEdge(0,4);
+	g1.addEdge(2,4);
+	g1.addEdge(3,2);
+	g1.addEdge(2,3);
+	g1.addEdge(3,5);
+
+    g1.BFS1(0);
 }
