9a838eb2ecb50fee950321d7050ca1e9e335d518
Aditya Sarin <aditya28sarin@gmail.com>
2021-03-29 20:46:42 +0530

    Update 129

--- DSA/Data Structures/Non-Linear DS/Graphs/Detect_Cycle_Directed.cpp
+++ DSA/Data Structures/Non-Linear DS/Graphs/Detect_Cycle_Directed.cpp
@@ -22,68 +22,40 @@ void addEdge(int v, int w)
 } 
 
 
-bool detectCycle()
-{
-    set<int> not_visited;
-    set<int>  visiting;
-    set<int>  visited;
+bool detectCycleUtil(vector<bool> visited, int curr){
+    if(visited[curr]==true){
+        return true;
+    }
 
-    for(int i=0;i<V;i++)
-    {
-        not_visited.insert(i);
-    }    
+    visited[curr]=true;
+    bool flag = false;
+    for(auto element: adj[curr]){
+        flag = detectCycleUtil(visited, element);
 
-    while(not_visited.size()>0)
-    {
-        int current = random(not_visited);
-        if(DFS(current, not_visited,visiting,visited))
-        {
+        if(flag==true){
             return true;
         }
     }
     return false;
 }
 
-int random(set<int> &st)
-{
-    set<int> :: iterator it = st.begin();
-    int random = rand() % st.size();
-    advance(it,random);
+bool detectCycle(){
 
-    return *it;
-}
+    vector<bool> visited(V,false);
+    bool flag=false;
 
-void moveVertex(int current,set<int> &not_visited,set<int> &visiting)
-{
-    not_visited.erase(current);
-    visiting.insert(current);
-}
-
-bool DFS(int current,set<int> &not_visited, set<int> &visiting, set<int> &visited)
-{
-    moveVertex(current, not_visited, visiting);
-
-    for(int neighbor: adj[current])
-    {
- 
-        
-        if(visited.find(neighbor)!=visited.end())
-            continue;
-
-        if(visiting.find(neighbor)!=visiting.end())
-            return true;
-
-        if(DFS(neighbor,not_visited,visiting,visited))
-        {
-            return true;
+    for(int i=0;i<V;i++){
+        visited[i]=true;
+        for(auto element: adj[i]){
+            flag = detectCycleUtil(visited, element);
+            if(flag==true){
+                return true;
+            }
         }
-       
+        visited[i]=false;
     }
-
-    moveVertex(current, visiting, visited);
-     return false;
+    return false;
 }
-
 
 };
 
@@ -93,10 +65,10 @@ int main()
         g.addEdge(0, 1); 
         g.addEdge(0, 3); 
         g.addEdge(1, 2); 
-        g.addEdge(0, 2); 
+        // g.addEdge(0, 2); 
         g.addEdge(3, 4); 
         g.addEdge(4, 5); 
-        g.addEdge(5, 3); 
+        // g.addEdge(5, 3); 
 
 	if(g.detectCycle()) 
         cout << "Graph contains cycle"; 
