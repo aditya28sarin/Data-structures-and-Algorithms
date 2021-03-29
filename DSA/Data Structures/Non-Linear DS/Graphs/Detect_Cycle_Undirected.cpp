6ffc16ee0f68e0939ef07b709330336d1443ffe7
Aditya Sarin <aditya28sarin@gmail.com>
2021-03-29 17:56:27 +0530

    Update 128

--- DSA/Data Structures/Non-Linear DS/Graphs/Detect_Cycle_Undirected.cpp
+++ DSA/Data Structures/Non-Linear DS/Graphs/Detect_Cycle_Undirected.cpp
@@ -1,5 +1,7 @@
 // URL: https://www.youtube.com/watch?v=eTaWFhPXPz4&t=1369s
 
+
+//Using Disjoint Set (Not optimized)
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -33,6 +35,51 @@ bool detectCycle(vector<pair<int,int>>& vt,vector<int>& dsuf, int V){
     return false;
 }
 
+
+//using Disjoint Set (With Path Compression and Union by Rank)
+struct node{
+    int parent;
+    int rank;
+};
+
+int find1(vector<node>& dsuf1, int element){
+    if(dsuf1[element].parent==-1){
+        return element;
+    }else{
+        return dsuf1[element].parent = find1(dsuf1,dsuf1[element].parent);
+    }
+}
+
+void unionDS1(vector<node>& dsuf1, int fromP, int toP){
+
+    if(dsuf1[fromP].rank > dsuf1[toP].rank){
+        dsuf1[toP].parent = fromP;
+    }else if(dsuf1[fromP].rank < dsuf1[toP].rank){
+        dsuf1[fromP].parent = toP;
+    }else{
+        dsuf1[fromP].parent = toP;   
+        dsuf1[toP].rank +=1;
+    }
+    
+}
+
+
+bool detectCycle1(vector<pair<int,int>>& vt, vector<node>& dsuf1, int V){
+    for(auto pair:vt){
+        
+        int fromP = find1(dsuf1,pair.first);
+        int toP= find1(dsuf1,pair.second);
+
+        if(fromP==toP){
+            return true;
+        }
+        unionDS1(dsuf1,fromP,toP);
+    }
+    return false;    
+}
+
+
+
 int main(){
 
     
@@ -46,8 +93,23 @@ int main(){
     vector<int> dsuf (V,-1);
 
     if(detectCycle(vt,dsuf,V)){
-        cout<<"Graph contains a cycle.";
+        cout<<"Graph 1 contains a cycle.";
     }else{
-        cout<<"Graph does not contain a cycle.";
+        cout<<"Graph 1 does not contain a cycle.";
+    }
+
+    vector<node> dsuf1;
+
+    dsuf.resize(V); 
+
+    for(int i=0;i<V;i++){
+        dsuf1[i].parent = -1;
+        dsuf1[i].rank = 0;
+    }
+
+    if(detectCycle1(vt,dsuf1,V)){
+        cout<<"Graph 2 contains a cycle.";
+    }else{
+        cout<<"Graph 2 does not contain a cycle.";
     }
 }
