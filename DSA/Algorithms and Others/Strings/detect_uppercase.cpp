2220434d1633ce9037e7afcf7530caec58b22bf5
Aditya Sarin <aditya28sarin@gmail.com>
2021-04-08 00:11:50 +0530

    Update 140

---
+++ DSA/Algorithms and Others/Strings/detect_uppercase.cpp
@@ -0,0 +1,34 @@
+// URL: https://leetcode.com/problems/detect-capital/
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.size();
+        
+        
+        if(n==1){
+            return true;
+        }
+        
+        if(isupper(word[0]) && isupper(word[1])){
+            for(int i=2;i<n;i++){
+                if(islower(word[i])){
+                    return false;
+                }
+            }
+        }else if(isupper(word[0]) && islower(word[1])){
+            for(int i=2;i<n;i++){
+                if(isupper(word[i])){
+                    return false;
+                }
+            }
+        }else if(islower(word[0])){
+            for(int i=1;i<n;i++){
+                if(isupper(word[i])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
