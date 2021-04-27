4141c2e8a92163b8597056774dc048d290129f5f
Aditya Sarin <aditya28sarin@gmail.com>
2021-04-27 15:28:33 +0530

    Update 162

--- DSA/Algorithms and Others/Searching/search_in_rotated_sorted_array.cpp
+++ DSA/Algorithms and Others/Searching/search_in_rotated_sorted_array.cpp
@@ -27,26 +27,26 @@ int findMin(int arr[], int n){
 }
 
 
-    int BinSearch(int arr[],int low, int high, int element){
-        
-        while(low<=high){
-            int mid = low + (high-low)/2;
+int BinSearch(int arr[],int low, int high, int element){
+    
+    while(low<=high){
+        int mid = low + (high-low)/2;
 
-            if(arr[mid]==element){
-                return mid;
-            }
-
-            else if(arr[mid]<=element){
-                low=mid+1;
-            }
-
-            else if(arr[mid]>element){
-                high=mid-1;
-            }
+        if(arr[mid]==element){
+            return mid;
         }
 
-        return -1;
+        else if(arr[mid]<=element){
+            low=mid+1;
+        }
+
+        else if(arr[mid]>element){
+            high=mid-1;
+        }
     }
+
+    return -1;
+}
 
 int main(){
     int arr[8]={11,12,15,18,2,5,6,8};
@@ -63,3 +63,82 @@ int main(){
         cout<<x;
     }
 }
+
+
+//new method 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l=0;
+        int r=n-1;
+        
+        
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(nums[m]>nums[r]){
+                l=m+1;
+            }else{
+                r=m;
+            }
+        }
+        
+        if(target<=nums[n-1]){
+            return bin_search(nums,target,l,n-1);
+        }else{
+            return bin_search(nums,target,0,l-1);
+        }
+    }
+    
+  
+    int bin_search(vector<int>& nums, int target, int l, int r){
+        
+        while(l<=r){
+            int mid = l +(r-l)/2;
+            
+            if(nums[mid]==target) return mid;
+            
+            else if(nums[mid]>target) r=mid-1;
+            
+            else l=mid+1;
+        }
+        
+        return -1;
+    }
+};
+
+
+//new method with duplicates
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        
+        while(l<=r){
+            int m=l+(r-l)/2;
+            
+            if(target==nums[m]) return true;
+            
+            else if(nums[m]==nums[l] && nums[m]==nums[r]){
+                l++;
+                r--;
+            }
+            
+            else if(nums[m]>=nums[l]){
+                if((target>=nums[l]) && (target<nums[m])){
+                    r=m-1;
+                }else{
+                    l=m+1;
+                }
+            }else{
+                if((target>nums[m]) && (target<=nums[r])){
+                    l=m+1;
+                }else{
+                    r=m-1;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
--- DSA/Algorithms and Others/Searching/times_sorted_array_rotated.cpp
+++ DSA/Algorithms and Others/Searching/times_sorted_array_rotated.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//old method
 int findRotation(int arr[], int n){
     int low=0;
     int high=n-1;
@@ -30,36 +32,54 @@ int findRotation(int arr[], int n){
     return -1;
 }
 
-
-//if duplicates
-
-class Solution {
-public:
-    int findMin(vector<int>& nums) {
-        int n = nums.size();
-        int low=0;
-        int high=n-1;
-        
-        while(low<high){
-            int mid = low + (high-low)/2;
-            
-            if(nums[mid]>nums[high]){
-                low=mid+1;
-            }
-            else if(nums[mid]<nums[high]){
-                high=mid;
-            }else{
-                high--;
-            }
-        }
-        
-        return nums[high];
-    }
-};
-
-
 int main(){
     int arr[8]={11,12,15,18,2,5,6,8};
 
     cout<<findRotation(arr,8);
 }
+
+//new method 
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l=0;
+        int r=nums.size()-1;
+        
+        while(l<r){
+            int m = l + (r-l)/2;
+            
+            if(nums[m]>nums[r]){
+                l=m+1;
+            }else if(nums[m]<nums[r]){
+                r=m;
+            }else{
+                return nums[l];
+            }
+        }
+        return nums[l];
+    }
+};
+
+
+//new method -> if duplicates
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l=0;
+        int r=nums.size()-1;
+        
+        while(l<r){
+            int m = l + (r-l)/2;
+            
+            if(nums[m]>nums[r]){
+                l=m+1;
+            }else if(nums[m]<nums[r]){
+                r=m;
+            }else{
+                r--;
+            }
+        }
+        return nums[r];
+    }
+};
+
