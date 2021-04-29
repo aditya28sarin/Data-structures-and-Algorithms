c65d707a322a656702e85f062bcafc0957a2a288
Aditya Sarin <aditya28sarin@gmail.com>
2021-04-29 13:20:20 +0530

    Update 168

---
+++ DSA/Data Structures/Linear DS/Arrays/1D Arrays/sign_of_product_of_array.cpp
@@ -0,0 +1,22 @@
+// URL: https://leetcode.com/problems/sign-of-the-product-of-an-array/
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            
+            if(nums[i]<0){
+                count++;
+            }else if(nums[i]==0){
+                return 0;
+            }
+        }
+        
+        if(count%2!=0){
+            return -1;
+        }
+        
+        return 1;
+    }
+};
