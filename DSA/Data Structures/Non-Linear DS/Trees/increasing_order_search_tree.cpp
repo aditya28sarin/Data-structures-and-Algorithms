fff0e2ad5d4c15c85ec2378c5e8bae65f71765d4
Aditya Sarin <aditya28sarin@gmail.com>
2021-05-02 23:18:49 +0530

    Update 177

---
+++ DSA/Data Structures/Non-Linear DS/Trees/increasing_order_search_tree.cpp
@@ -0,0 +1,25 @@
+// URL: https://leetcode.com/problems/increasing-order-search-tree/
+
+class Solution {
+public:
+    
+    vector<TreeNode*> v;
+void inorder(TreeNode* root){     
+    if(root){
+        inorder(root->left);
+        v.push_back(root);
+        inorder(root->right);
+    }
+}
+TreeNode* increasingBST(TreeNode* root) {
+    inorder(root);
+    for(int i = 0; i<v.size()- 1; i++){
+        v[i]->left = NULL;
+        v[i]->right = v[i+ 1];
+    }
+    v[v.size()- 1]->left = NULL;
+    v[v.size()- 1]->right = NULL;
+    return v[0];
+}
+};
+
