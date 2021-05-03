7d123ee4fedcf4bdee7876cc4a6472b62f084819
Aditya Sarin <aditya28sarin@gmail.com>
2021-05-03 22:03:17 +0530

    Update 181

---
+++ DSA/Data Structures/Non-Linear DS/Trees/average_of_levels_in_BT.cpp
@@ -0,0 +1,36 @@
+// URL: https://leetcode.com/problems/average-of-levels-in-binary-tree/
+
+
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        
+        vector<double> ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int n = q.size();
+            double sum=0;
+            for(int i=0;i<n;i++){
+                TreeNode* x = q.front();
+                q.pop();
+                if(x->left){
+                    q.push(x->left);
+                }
+                
+                if(x->right){
+                    q.push(x->right);
+                }
+                sum+=x->val;
+            }
+            
+            ans.push_back(sum*1.0/n);
+            
+        }
+        
+        return ans;
+        
+    }
+};
