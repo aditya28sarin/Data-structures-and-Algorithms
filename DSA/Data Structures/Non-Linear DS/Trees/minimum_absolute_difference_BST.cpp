6c8fd0b220eb8d2af5e438fe2514637de74c3bbc
Aditya Sarin <aditya28sarin@gmail.com>
2021-05-27 13:31:44 +0530

    Update 212

---
+++ DSA/Data Structures/Non-Linear DS/Trees/minimum_absolute_difference_BST.cpp
@@ -0,0 +1,17 @@
+// URL: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+
+class Solution {
+public:
+  
+    int min_dif = INT_MAX, val = -1;    
+int getMinimumDifference(TreeNode* root) {
+    if (root->left != NULL) 
+        getMinimumDifference(root->left);
+    if (val >= 0) 
+        min_dif = min(min_dif, root->val - val);
+    val = root->val;
+    if (root->right != NULL) 
+        getMinimumDifference(root->right);
+    return min_dif;
+}
+};
