e62af736d015d53996e227f0b60d09bf4df44021
Aditya Sarin <aditya28sarin@gmail.com>
2021-06-06 00:27:54 +0530

    Update 22

---
+++ DSA/Data Structures/Linear DS/Hashing/majority_element.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++; 
+            
+            if(mp[nums[i]]>n/2){
+                return nums[i];
+            }
+        }
+        
+        return 0;
+    }
+};
