451b106d4ff4263b9fc2cdb4a47063b7a53b5bf6
Aditya Sarin <aditya28sarin@gmail.com>
2021-07-05 23:52:42 +0530

    Update 246

--- DSA/Algorithms and Others/Searching/binary_search_in_rotated_sorted_array.cpp
+++ DSA/Algorithms and Others/Searching/binary_search_in_rotated_sorted_array.cpp
@@ -1,86 +1,74 @@
-// ou are given an integer array nums sorted in ascending order, and an integer target.
-
-// Suppose that nums is rotated at some pivot unknown to you beforehand (i.e., [0,1,2,4,5,6,7]
-// might become [4,5,6,7,0,1,2]).
-
-// If target is found in the array return its index, otherwise, return -1.
-
- 
-
-// Example 1:
-
-// Input: nums = [4,5,6,7,0,1,2], target = 0
-// Output: 4
-// Example 2:
-
-// Input: nums = [4,5,6,7,0,1,2], target = 3
-// Output: -1
-// Example 3:
-
-// Input: nums = [1], target = 0
-// Output: -1
- 
-
-// Constraints:
-
-// 1 <= nums.length <= 5000
-// -10^4 <= nums[i] <= 10^4
-// All values of nums are unique.
-// nums is guranteed to be rotated at some pivot.
-// -10^4 <= target <= 10^4
-
-
--------------------------------------------------------------------------------------------------------
-						CODE
--------------------------------------------------------------------------------------------------------
-
-#include<bits/stdc++.h>
-using namespace std;
-
-//searching is done using binary search
-int searchRotatedSortedArray(vector<int>& nums, int target) {
+class Solution {
+public:
+    
+     int findMin(vector<int>& nums,int target) {
+       int l=0;
+        int r=nums.size()-1;
+        int n=nums.size();
+        int pv=0;
+        while(l<=r){
+            
+            int m = l + (r-l)/2;
+            int prev=(m+n-1)%n;
+            int next=(m+1)%n;
+            
+            if(nums[l]<=nums[r]) return l;
+            
+            if(nums[m]<=nums[prev] && nums[m]<=nums[next]){
+                pv=m;
+                break;
+            }
+            
+            if(nums[m]>=nums[l]){
+                l=m+1;
+            }
+            
+            else if(nums[m]<=nums[r]){
+                r=m-1;
+            }
+           
+        }
         
-		int l = 0;
-        int r = nums.size()-1;
-        while(l <= r) {
-            int mid = (l+r)/2;
-            if(target == nums[mid]) {
-                return mid;
-            } 
+        return pv;
+    }
+    
+    
+    int binSearch(vector<int>& nums, int target, int l, int r){
+        
+        
+        while(l<=r){
+            int m=l+(r-l)/2;
             
+            if(nums[m]==target){
+                return m;
+            }
             
-            if(nums[l] <= nums[mid]) {
-                if(target > nums[mid] || target < nums[l]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            } else {
-                if(target < nums[mid] || target > nums[r]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
+            else if(target>=nums[m]){
+                l=m+1;
+            }
+            
+            else if(target<=nums[m]){
+                r=m-1;
             }
         }
+        
         return -1;
     }
-
-int main()
-{
-    int size_of_array;
-    cin>>size_of_array;
-
-    vector<int> input_array(0,size_of_array)
-    for(int i=0;i<size_of_array;i++)
-    {
-        int temp;
-		cin>>temp;
-		input_array.push_back(temp);
+    
+    int search(vector<int>& nums, int target) {
+        
+        int idx=findMin(nums,target);
+        
+        if(nums[idx]==target) return idx;
+        
+        int val1=0,val2=0;
+        val1=binSearch(nums,target,0,idx-1);
+        val2=binSearch(nums,target,idx,nums.size()-1);
+        
+        
+        return val1 > val2? val1 : val2;
+        
+        return val1;
+        
     }
-
-    int element;
-    cin>>element;
-
-    cout<<searchRotatedSortedArray(input_array,element);
-}
+};
